552587fcfc96e2e208a4377bba06b29452573499
Veselov Andrey <Aveselov88@gmail.com>
2023-07-07 20:58:06 +0300

    мастер4

--- lib/main.dart
+++ lib/main.dart
@@ -96,7 +96,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'qqw2',
+              'мастер4',
             ),
             Text(
               '$_counter',
